8a11deb9a98b4e6f79e53bfcaa210a8ff912b2cd
donsonioc2010 <whddnjs822@gmail.com>
2021-09-12 22:04:05 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+알고리즘 팠티
+<br><br>
+Mr.Boguem, Mr.SungBo, Mr.Jong1, Mr.Sunghee
---
+++ src/main.java
@@ -0,0 +1,7 @@
+import week1.munje1;
+
+public class main {
+    public static void main(String[] args) {
+        week1.munje2 a = new week1.munje2();
+    }
+}
